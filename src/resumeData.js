4c7fadf3037ec69bd9ec089bb463e24e87f1935f
ali saee <saeeali1979@gmail.com>
2020-06-27 15:42:43 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "role": "Junior Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/ali-saee-13431658/",
+    
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        }
+      ],
+
+    "aboutme":"My background study is completely different regards to what am I doing now. I familiarized myself coincidentally with the programming language and the realized that I really like it because I can be a creative person who can make different things.",
+    
+    "education":[
+      {
+        "CompanyName":"Mano",
+        "StyudyField ":"Programming Language(Front-End)",
+        "YearOfStart":"April 2020",
+        "Achievements":"HTML5, CSS3, JvaScript, Git, React",
+      },
+      {
+        "UniversityName":"Azad University",
+        "StyudyField ":"European Studies",
+        "YearOfPassing":"2010",
+        "Achievements":"Master Degree",
+      },
+      {
+        "UniversityName":"Azad University",
+        "StyudyField ":"Political Science",
+        "YearOfPassing":"2003",
+        "Achievements":"Bachelor Degree",
+      }
+    ],
+
+    "work":[
+      {
+        "CompanyName":"Syncreon",
+        "JobTitle":"Operator",
+        "YearOfStart":"Mar 2020",
+        "YearOfLeaving":"Present",
+        "Location": "Netherlands",
+      },
+      {
+        "CompanyName":"Nippon Express",
+        "JobTitle":"Administrative Assistant",
+        "YearOfStart":"Apr 2019",
+        "YearOfLeaving":"Nov 2019",
+        "Location": "Netherlands",
+      },
+      {
+        "CompanyName":"Syncreon",
+        "JobTitle":"Operator",
+        "YearOfStart":"Apr 2018",
+        "YearOfLeaving":"Feb 2019",
+        "Location": "Netherlands",
+      },
+      {
+        "CompanyName":"Telephone Servery Research",
+        "JobTitle":"Operator",
+        "YearOfStart":"May 2017",
+        "YearOfLeaving":"Dec 2017",
+        "Location": "Netherlands",
+      },
+      {
+        "CompanyName":"HollandGate",
+        "JobTitle":"Administrative Assistant",
+        "YearOfStart":"Jan 2016",
+        "YearOfLeaving":"Mar 2017",
+        "Location": "Netherlands",
+      },
+      {
+        "CompanyName":"Customer Service",
+        "JobTitle":"Telecommunication Company",
+        "YearOfStart":"Sep 2003",
+        "YearOfLeaving":"Dec 2013",
+        "Location": "Iran",
+      }
+
+    ],
+
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS3"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"html&css",
+        "imgurl":"images/pic1.png"
+      },
+      {
+        "name":"project2",
+        "description":"javascript",
+        "imgurl":"images/pic2.png"
+      },
+      {
+        "name":"project3",
+        "description":"jqury",
+        "imgurl":"images/pic3.png"
+      },
+      {
+        "name":"project4",
+        "description":"react",
+        "imgurl":"images/pic4.png"
+      }
+    ]
+  }
+  
+  export default resumeData
